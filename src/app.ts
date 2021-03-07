1ce3f2b610932d689bd4013715c22a4c28c4fbaa
John Chuang <wchih.john@gmail.com>
2021-03-07 21:52:38 +0800

    1. use cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import cors = require('cors');
 import routes from './routes/routes';
 
 const EXPRESS_APP: express.Application = express();
 
 EXPRESS_APP.use(bodyParser.json());
 EXPRESS_APP.use(bodyParser.urlencoded({ extended: false }));
+EXPRESS_APP.use(cors());
+
+EXPRESS_APP.use('/', express.static('public'))
+
 
 for (const route of routes) {
   EXPRESS_APP.use(route.getApiPrefix(), route.getRouter());
